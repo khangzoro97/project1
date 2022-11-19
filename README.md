b2f0dc6d313a680f88d18686132ff8954118e780
khang <khangzoro97@gmail.com>
2022-11-19 17:38:21 +0700

    submit 3 commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Vuejs
+
+1, Cài đặt
+
+2, tạo component và blinding data
+
+3,devtool
+
+4, Method trong vuejs
+
+5, Computed. Sự khác nhau với method
+
+6, test commit
+
+7, test commit 2
---
+++ resources/js/components/EventHandling.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="event-handling">
+        <div @click.self="alertHello">
+            <button @click.exact="onClick">A</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        onClick() {
+            alert('Hello')
+        },
+        alertWelcome() {
+            alert('Welcome')
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
