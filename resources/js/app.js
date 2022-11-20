6441d15dc9d8e4ed055eb47816338221111545cf
khang <khangzoro97@gmail.com>
2022-11-20 23:10:54 +0700

    code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import { createApp } from 'vue'
+import Welcome from './components/MyComponent'
+
+const app = createApp({})
+
+app.component('welcome', Welcome)
+
+app.mount('#app')
---
+++ resources/js/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="my-component">
+        <div>{{ reverseMessage }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'This is my first component using binding data'
+        }
+    },
+    computed: {
+        reverseMessage() {
+            return this.message.split('').reverse().join('')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.my-component {
+    color: red;
+}
+</style>
